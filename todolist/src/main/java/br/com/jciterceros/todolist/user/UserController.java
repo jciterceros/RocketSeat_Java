87a7e25940f81978d183f3814f47af26d54559ce
jciterceros <jciterceros@yahoo.com>
2023-10-09 20:51:10 -0300

    create UserController and UserModel

---
+++ todolist/src/main/java/br/com/jciterceros/todolist/user/UserController.java
@@ -0,0 +1,26 @@
+package br.com.jciterceros.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificador
+ * public: significa que a classe UserController pode ser acessada por qualquer outra classe
+ * private: significa que a classe UserController só pode ser acessada por ela mesma
+ * protected: significa que a classe UserController pode ser acessada por ela mesma e por classes que a estendem
+*/
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /*
+     * Body: é o corpo da requisição, onde você envia os dados que deseja enviar
+     * para o servidor
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+}
---
+++ todolist/src/main/java/br/com/jciterceros/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.jciterceros.todolist.user;
+
+public class UserModel {
+    public String name;
+    public String Username;
+    public String password;
+}
