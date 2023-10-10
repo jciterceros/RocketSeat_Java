76e6f0496dbb97063a4044df27a32e971411f1c4
jciterceros <jciterceros@yahoo.com>
2023-10-10 20:56:54 -0300

    add Repository

--- todolist/src/main/java/br/com/jciterceros/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/jciterceros/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.jciterceros.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     /*
      * Body: é o corpo da requisição, onde você envia os dados que deseja enviar
      * para o servidor
      */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        // System.out.println(userModel.getUsername());
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
