50816f4ea48114dd78a1d041c82adc582ce3e356
jciterceros <jciterceros@yahoo.com>
2023-10-10 21:27:36 -0300

    validate username

--- todolist/src/main/java/br/com/jciterceros/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/jciterceros/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package br.com.jciterceros.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,9 +27,15 @@ public class UserController {
      * para o servidor
      */
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         // System.out.println(userModel.getUsername());
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
