759f95a168b93fd58dd3d7a876309defa5e1ee5b
jciterceros <jciterceros@yahoo.com>
2023-10-11 19:46:22 -0300

    add security with hash

--- todolist/src/main/java/br/com/jciterceros/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/jciterceros/todolist/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /*
  * Modificador
  * public: significa que a classe UserController pode ser acessada por qualquer outra classe
@@ -34,6 +36,9 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
