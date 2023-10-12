ea648e44db055d22ff03819c27ebe88f80fd6a5c
jciterceros <jciterceros@yahoo.com>
2023-10-11 21:40:16 -0300

    Create FiltertaskAuth.java

---
+++ todolist/src/main/java/br/com/jciterceros/todolist/filter/FiltertaskAuth.java
@@ -0,0 +1,58 @@
+package br.com.jciterceros.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.jciterceros.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FiltertaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Pegar a autenticação (usuario e senha)
+        var authorization = request.getHeader("Authorization");
+        System.out.println("Authorization: " + authorization);
+
+        var authEncoded = authorization.substring("Basic ".length()).trim();
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+        var authString = new String(authDecode);
+        String[] credentials = authString.split(":");
+        var username = credentials[0];
+        var password = credentials[1];
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+
+        // Validar usuario
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            System.out.println("User: " + user.getUsername());
+            // Validar senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (!passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+            // Segue o fluxo
+        }
+    }
+
+}
